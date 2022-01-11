8fceefad784b8d07b0f8826cae98a563eed7baab
Mandeep Debnath <mandeepdebnath@gmail.com>
2022-01-11 18:06:03 +0530

    adding new post page

--- src/script.js
+++ src/script.js
@@ -2,15 +2,15 @@ let btn = document.querySelector(".toggle");
 let icon = document.querySelector(".fa-bars");
 let nav = document.querySelector(".show-menu");
 
-btn.onclick = function () {
-  if (icon.classList.contains("fa-bars")) {
-    icon.classList.replace("fa-bars", "fa-times");
-    nav.style.display = "block";
-  } else {
-    icon.classList.replace("fa-times", "fa-bars");
-    nav.style.display = "none";
-  }
-};
+// btn.onclick = function () {
+//   if (icon.classList.contains("fa-bars")) {
+//     icon.classList.replace("fa-bars", "fa-times");
+//     nav.style.display = "block";
+//   } else {
+//     icon.classList.replace("fa-times", "fa-bars");
+//     nav.style.display = "none";
+//   }
+// };
 
 let jobField = document.querySelector(".skill-content-job");
 
